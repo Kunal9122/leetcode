37a0495ab1a5bd63141de7b13a71444b46d0f884
Kunal9122 <kunalyadav647441@gmail.com>
2025-10-22 22:31:06 +0530

    Time: 4 ms (41.24%), Space: 14.9 MB (40.09%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string str="";
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' '){
+                str+=s[i];
+            }
+            else{
+                reverse(str.begin(),str.end());
+                ans+=str;
+                ans+=' ';
+                str="";
+            }
+        }
+        reverse(str.begin(),str.end());
+        ans+=str;
+        return ans;
+    }
+};
