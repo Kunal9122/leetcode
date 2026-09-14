97f4e6ba7de2a227e1cc21c8c00a67dd051fd127
Kunal yadav <kunalyadav647441@gmail.com>
2026-09-15 00:34:27 +0530

    Time: 0 ms (100%), Space: 28.9 MB (92.58%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
-        int n = nums.size();
-        if(k == n) {
-            return *max_element(nums.begin(), nums.end());
+        int c1=0,c2=0;
+        int n=nums.size();
+        int mx=nums[0];
+        if(n==k){
+            for(int i=0;i<n;i++) mx=max(nums[i],mx);
+            return mx;
         }
-        if(k == 1) {
-            unordered_map<int,int> mp;
-
-            for(int x : nums)
-                mp[x]++;
-
-            int ans = -1;
-
-            for(auto x : mp) {
-                if(x.second == 1)
-                    ans = max(ans, x.first);
+        if(k==1){
+            unordered_map<int,int>mp;
+            for(int i=0;i<n;i++) mp[nums[i]]++;
+            int mx=-1;
+            while(mp.size()){
+                auto x=mp.begin();
+                if(x->second==1){
+                    mx=max(mx,x->first);
+                }
+                mp.erase(x);
             }
-
-            return ans;
+            return mx;
         }
-
-        int c1 = 0, c2 = 0;
-        for(int i = 1; i < n; i++) {
-            if(nums[i] == nums[0])
-                c1++;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[0]) c1++;
         }
-        for(int i = 0; i < n - 1; i++) {
-            if(nums[i] == nums[n-1])
-                c2++;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[nums.size()-1]) c2++;
         }
-        int ans = -1;
-        if(c1 == 0)
-            ans = max(ans, nums[0]);
-        if(c2 == 0)
-            ans = max(ans, nums[n-1]);
-        return ans;
+        if(c1 > 0 && c2>0) return -1;
+        if(c1<c2){
+            return nums[0];
+        }
+        if(c2<c1) return nums[n-1];
+        else return max(nums[0],nums[n-1]);
     }
 };
