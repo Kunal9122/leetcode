1e1b1b2549f8eb8f8da6d9383fb1b6620219ce2c
Kunal yadav <kunalyadav647441@gmail.com>
2026-09-19 00:27:01 +0530

    Time: 18 ms (8.98%), Space: 14 MB (75.02%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0) continue;
+            else{
+                for(int j=n-2;j>=i+1;j--){
+                    arr[j+1]=arr[j];
+                }
+                if(i+1<n)
+                arr[i+1]=0;
+                i++;
+            }
+    }
+        return;
+    }
+};
