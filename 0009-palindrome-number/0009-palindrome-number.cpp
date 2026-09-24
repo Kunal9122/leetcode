95eb2734b8ee7b5ca6fe295b0bb157c8afe81a9c
Kunal yadav <kunalyadav647441@gmail.com>
2026-09-24 23:38:39 +0530

    Time: 0 ms (100%), Space: 8.4 MB (91.79%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int c=x;
+        long long ans=0;
+        while(x>0){
+            int r=x%10;
+            ans=ans*10+r;
+            x=x/10;
+        }
+        if(ans==c) return true;
+        return false;
+    }
+};
